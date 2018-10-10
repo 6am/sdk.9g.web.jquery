f57173cec48d7d51afca274b300ae80828e071b9
Tiago Santos <tiagoa.santos@hotmail.com>
2018-10-10 11:39:47 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php /* ]
-apikey = "DEVBGOad8xdpibiODYYsWMFQoXzMzf";
-apitoken =  "ad8xdpibiODYYsWMFQoXzMzfUbVOdTW5";
-accesstoken =  "BGOPROD73FB48DFBDA2C4AB341A9689D52F7";
+apikey = "";
+apitoken =  "";
+accesstoken =  "";
 siteid =  "1";
 end] */ ?>
